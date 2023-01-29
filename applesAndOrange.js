294f976f56470b4272df04954ae4636dad72866b
Felipe2304 <felipeaxandre936@gmail.com>
2023-01-29 08:29:25 +0044

    refatorando o desafio applesAndOranges

--- applesAndOrange.js
+++ applesAndOrange.js
@@ -1,20 +1,11 @@
-const checkedResult = (a, b) => {
-  if (a === b) console.log("resultado correto");
-  else console.log(`o resultado ${a}, não se concede com o ${b}`);
-};
-
 const countApplesAndOranges = (s, t, a, b, apples, oranges) => {
-  const NApples = apples
-    .map((applesValues) => {
-      return applesValues + a;
-    })
-    .filter((applesValues) => applesValues >= s && applesValues <= t).length;
+  const NApples = apples.filter(
+    (applesValues) => applesValues + a >= s && applesValues + a <= t
+  ).length;
 
-  const NOranges = oranges
-    .map((orangesValues) => {
-      return orangesValues + b;
-    })
-    .filter((orangesValues) => orangesValues >= s && orangesValues <= t).length;
+  const NOranges = oranges.filter(
+    (orangesValues) => orangesValues + b >= s && orangesValues + b <= t
+  ).length;
 
   console.log(NApples);
   console.log(NOranges);
@@ -23,6 +14,4 @@ const countApplesAndOranges = (s, t, a, b, apples, oranges) => {
 const apples = [-2, 2, 1];
 const oranges = [5, -6];
 
-const result = countApplesAndOranges(7, 11, 5, 15, apples, oranges);
-
-checkedResult((1, 1), result);
+countApplesAndOranges(7, 11, 5, 15, apples, oranges);
